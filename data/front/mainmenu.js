ff650a05b00f4abd56a968c196ae473da5b47276
Mikhail Butalin <mail@zomgdev.com>
2026-02-13 00:38:49 +0300

    202602130038 Чего-бы сделать?

--- data/front/mainmenu.js
+++ data/front/mainmenu.js
@@ -1,11 +1,11 @@
 {
-  "Архитектура": ["Infra layer", "Management layer", "Data layer", "Process layer"],
-  "Resources/Assets": ["Hardware", "Software", "Misc"],
-  "Infra": ["Servers", "VMs", "Services", "Environments"],
-  "Manage": ["Projects", "Groups"],
-  "Processes": {
+  "1. Архитектура": ["Discovery", "Infra layer", "Management layer", "Data layer", "Process layer"],
+  "2. Resources/Assets": ["Hardware", "Software", "Misc"],
+  "3. Infra": ["Servers", "VMs", "Services", "Environments"],
+  "4. Manage": ["Projects", "Groups"],
+  "5. Processes": {
     "Workflow": ["install", "update", "uninstall"],
     "Actions": ["start", "stop", "restart"]
   },
-  "Assets": ["ОС", "Нематериальные"]
+  "6. Assets": ["ОС", "Нематериальные"]
 }
